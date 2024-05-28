74b0e0a284ce4855dbe853de164dd8a0a55d5274
KyleLin <583154233@qq.com>
2024-05-28 10:13:25 +0800

    【实战系列 二】

---
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Realm/C2R_LoginAccountHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ET.Server;
+
+[MessageHandler(SceneType.Realm)]
+public class C2R_LoginAccountHandler:MessageSessionHandler<C2R_LoginAccount,R2C_LoginAccount>
+{
+    protected override async ETTask Run(Session session, C2R_LoginAccount request, R2C_LoginAccount response)
+    {
+        session.RemoveComponent<SessionAcceptTimeoutComponent>();
+
+        if (session.GetComponent<SessionLockingComponent>()!=null)
+        {
+            response.Error = ErrorCode.ERR_RequestRepeatedly;
+            session.Disconnect().Coroutine();
+            return;
+        }
+
+        if (string.IsNullOrEmpty(request.AccountName) || string.IsNullOrEmpty(request.Password))
+        {
+            response.Error = ErrorCode.ERR_LoginInfoIsNull;
+            session.Disconnect().Coroutine();
+            return;
+        }
+
+        if (!Regex.IsMatch(request.AccountName.Trim(), @"^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*).{6.15}$"))
+        {
+            response.Error = ErrorCode.ERR_AccountNameFormError;
+            session.Disconnect().Coroutine();
+            return;
+        }
+        
+        if (!Regex.IsMatch(request.Password.Trim(), @"^[A-za-z0-9]+$"))
+        {
+            response.Error = ErrorCode.ERR_PasswordFormError;
+            session.Disconnect().Coroutine();
+            return;
+        }
+
+        CoroutineLockComponent coroutineLockComponent = session.Root().GetComponent<CoroutineLockComponent>();
+        using (session.AddComponent<SessionLockingComponent>())
+        {
+            using (await coroutineLockComponent.Wait(CoroutineLockType.LoginAccount,request.AccountName.GetLongHashCode()))
+            {
+                
+            }
+        }
+    }
+}
--- Unity/Assets/Scripts/Model/Share/Module/Message/ErrorCode.cs
+++ Unity/Assets/Scripts/Model/Share/Module/Message/ErrorCode.cs
@@ -13,5 +13,11 @@ namespace ET
         // 这里配置逻辑层的错误码
         // 110000 - 200000是抛异常的错误
         // 200001以上不抛异常
+
+
+        public const int ERR_RequestRepeatedly = 200001;
+        public const int ERR_LoginInfoIsNull = 200002;
+        public const int ERR_AccountNameFormError = 200003;
+        public const int ERR_PasswordFormError = 200004;
     }
 }
