8e7fd2414a5fa9058a80c08ecda90f20afbc3da4
KyleLin <583154233@qq.com>
2024-05-28 10:16:32 +0800

    【实战系列 二】命名空间的声明方式

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Realm/DisConnectHelper.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Realm/DisConnectHelper.cs
@@ -1,22 +1,24 @@
-﻿namespace ET.Server;
-
-public static class DisConnectHelper
+﻿namespace ET.Server
 {
-    public static async ETTask Disconnect(this Session self)
+    public static class DisConnectHelper
     {
-        if (self == null || self.IsDisposed)
+        public static async ETTask Disconnect(this Session self)
         {
-            return;
-        }
+            if (self == null || self.IsDisposed)
+            {
+                return;
+            }
 
-        long instanceId = self.InstanceId;
-        TimerComponent timerComponent = self.Root().GetComponent<TimerComponent>();
-        await timerComponent.WaitAsync(1000);
+            long instanceId = self.InstanceId;
+            TimerComponent timerComponent = self.Root().GetComponent<TimerComponent>();
+            await timerComponent.WaitAsync(1000);
 
-        if (self.InstanceId != instanceId)
-        {
-            return;
+            if (self.InstanceId != instanceId)
+            {
+                return;
+            }
+            self.Dispose();
         }
-        self.Dispose();
     }
 }
+
