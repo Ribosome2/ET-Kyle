5a3b2c5758def3ba284b579cd379b9a25ed91fc0
Ribosome2 <583154233@qq.com>
2024-06-24 11:15:16 +0800

    EUI

---
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/DlgMain/DlgMainSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET.Client
+{
+	[FriendOf(typeof(DlgMain))]
+	public static  class DlgMainSystem
+	{
+
+		public static void RegisterUIEvent(this DlgMain self)
+		{
+		 
+		}
+
+		public static void ShowWindow(this DlgMain self, Entity contextData = null)
+		{
+		}
+
+		 
+
+	}
+}
--- Unity/Assets/Scripts/ModelView/Client/Module/UI/UIComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/Module/UI/UIComponent.cs
@@ -35,5 +35,11 @@ namespace ET.Client
 				this.uiGlobalComponent = value;
 			}
 		}
+		
+		public Dictionary<int, UIBaseWindow> AllWindowsDic     = new Dictionary<int, UIBaseWindow>();
+		public List<WindowID> UIBaseWindowlistCached           = new List<WindowID>();
+		public Dictionary<int, UIBaseWindow> VisibleWindowsDic = new Dictionary<int, UIBaseWindow>();
+		public Queue<WindowID> StackWindowsQueue               = new Queue<WindowID>();
+		public bool IsPopStackWndStatus                        = false;
 	}
 }
